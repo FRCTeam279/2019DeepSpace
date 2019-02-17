fa768538848afd60192fa00fafbd019d7509c4eb
Ahmed H. EL-Shaer <aheshaer@gmail.com>
2019-02-17 17:46:57 -0500

    AHES: made changes to autoclimb.py

--- commands/autoclimb.py
+++ commands/autoclimb.py
@@ -8,7 +8,7 @@ from commands.extendfront import ExtendFront
 from commands.retractall import RetractAll   # used to be retractfront
 
 class AutoClimb(CommandGroup):
-
+    climbSpd = .4
     def __init__(self):
             super().__init__('AutoClimb')
             self.setInterruptible(True)
@@ -17,25 +17,25 @@ class AutoClimb(CommandGroup):
             self.addSequential(PrintCommand("Starting Climb"))
 
             self.addSequential(ExtendBack())    
-            self.addSequential(WaitCommand(0.5))
+            self.addSequential(WaitCommand(0.5))  # time-to-run = .5 sec
 
-            self.addSequential(DriveRaw(-.25,-.25, 1))  
-            self.addSequential(WaitCommand(1))
+            self.addSequential(DriveRaw(-self.climbSpd,-self.climbSpd, 1))  
+            self.addSequential(WaitCommand(1))    # time-to-run = 1.0 sec
                 
             self.addSequential(ExtendFront())    
-            self.addSequential(WaitCommand(0.5))
+            self.addSequential(WaitCommand(0.5))  # time-to-run = .5 sec
             
             self.addSequential(RetractBack())    
-            self.addSequential(WaitCommand(2))
+            self.addSequential(WaitCommand(2))   
 
-            self.addSequential(DriveRaw(-.25,-.25, 2))    
-            self.addSequential(WaitCommand(1))
+            self.addSequential(DriveRaw(-self.climbSpd,-self.climbSpd, 1))    
+            self.addSequential(WaitCommand(1.2))
 
             self.addSequential(RetractAll())    
             self.addSequential(WaitCommand(3))
 
-            self.addSequential(DriveRaw(-.25,-.25, 1))    
-            self.addSequential(WaitCommand(1))
+            self.addSequential(DriveRaw(-self.climbSpd,-self.climbSpd, 1))    
+            self.addSequential(WaitCommand(1.2))
 
             self.addSequential(PrintCommand("Finished Test"))
             
